cce8200bce82457631d365aa1a6cddca94ead890
stevenqha <35740363+stevenqha@users.noreply.github.com>
2018-11-17 20:06:30 -0500

    Add files via upload

---
+++ Caliper.ino
@@ -0,0 +1,133 @@
+#include <Elegoo_GFX.h>    // Core graphics library
+#include <Elegoo_TFTLCD.h> // Hardware-specific library
+#include <TouchScreen.h>
+
+#if defined(__SAM3X8E__)
+    #undef __FlashStringHelper::F(string_literal)
+    #define F(string_literal) string_literal
+#endif
+
+#define YP A3  // must be an analog pin, use "An" notation!
+#define XM A2  // must be an analog pin, use "An" notation!
+#define YM 9   // can be a digital pin
+#define XP 8   // can be a digital pin
+
+//Touch For New ILI9341 TP
+#define TS_MINX 120
+#define TS_MAXX 900
+
+#define TS_MINY 70
+#define TS_MAXY 920
+
+// For better pressure precision, we need to know the resistance
+// between X+ and X- Use any multimeter to read it
+// For the one we're using, its 300 ohms across the X plate
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+
+#define LCD_CS A3
+#define LCD_CD A2
+#define LCD_WR A1
+#define LCD_RD A0
+#define LCD_RESET A4
+
+// Assign human-readable names to some common 16-bit color values:
+#define BLACK   0x0000
+#define BLUE    0x001F
+#define RED     0xF800
+#define GREEN   0x07E0
+#define CYAN    0x07FF
+#define MAGENTA 0xF81F
+#define YELLOW  0xFFE0
+#define WHITE   0xFFFF
+
+Elegoo_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+//Tare Button
+#define TARE_L  100
+#define TARE_W  60
+#define TARE_X  110
+#define TARE_Y  180
+
+//Length Text
+#define TEXT_X 20
+#define TEXT_Y 60
+
+#define POT   A5
+int potValue = 0;
+int tare_value = 0;
+double length_ = 0;
+
+#define TEXT_LEN 8
+char textfield[TEXT_LEN+1] = "";
+uint8_t textfield_i=0;
+
+void setup() {
+  pinMode(POT, INPUT);
+  Serial.begin(9600);
+  tft.reset();
+  
+  uint16_t identifier = tft.readID();
+  if(identifier != 0x9325 || identifier != 0x9328 || identifier != 0x4535 || identifier != 0x7575 || identifier != 0x9341 || identifier != 0x8357 || identifier==0x0101)
+  {    
+      identifier=0x9341;
+  }
+  tft.begin(identifier);
+  tft.setRotation(1);
+
+  tft.fillScreen(BLACK);
+
+  tft.setTextColor(BLACK); 
+  tft.setTextSize(4);
+  tft.fillRect(TARE_X, TARE_Y, TARE_L, TARE_W, YELLOW);
+  tft.setCursor(114, 196);
+  tft.print ("TARE");
+  tft.setTextSize(5);
+  tft.setTextColor(WHITE);
+  tft.setCursor(240, TEXT_Y);
+  tft.print ("mm");
+
+}
+
+void loop() {
+  potValue = 4*analogRead (POT); 
+
+  TSPoint p = ts.getPoint();
+
+  // if sharing pins, you'll need to fix the directions of the touchscreen pins
+  pinMode(XM, OUTPUT);
+  pinMode(YP, OUTPUT);
+
+  if (p.z > MINPRESSURE && p.z < MAXPRESSURE) { 
+    int temp_py = p.y;
+    
+    // scale touch screen coordinates to actual coordinates
+    p.y = map(p.x, TS_MINX, TS_MAXX, tft.height(), 0);
+    p.x = map(temp_py, TS_MINY, TS_MAXY, tft.width(), 0);
+
+    //Check if button has been pressed
+    if (p.x < (TARE_X + TARE_L) && p.x > TARE_X && p.y < (TARE_Y+TARE_W) && p.y > TARE_Y) {
+      tare_value = potValue;
+    }
+  }
+
+  length_ = 0.03488 * (potValue - tare_value);
+  tft.setCursor(TEXT_X, TEXT_Y);
+  tft.fillRect(TEXT_X,TEXT_Y,210,35, BLACK); //Clear old text with a black rectangle
+
+  //Pad with appropriate number of spaces depending on the size of the number
+  if (length_ >= 100)
+    tft.print (" ");
+  else if (length_ < 100 && length_ >= 10)
+    tft.print ("  ");
+  else if (length_ < 10 && length_ >= 0)
+    tft.print ("   ");
+  else{
+    length_ = length_*-1;
+    tft.print ("-  ");  
+  }
+
+  tft.print (length_);
+  delay (100);
+}
